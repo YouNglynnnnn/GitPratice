2d0388599e1318e23378b90b30902feb3a719ec7
yanglin <lyn00009@163.com>
2017-06-25 15:29:26 +0800

    origin提交

--- hellogit/src/com/hellogit/HelloGit.java
+++ hellogit/src/com/hellogit/HelloGit.java
@@ -1,10 +1,13 @@
 package com.hellogit;
 
+import com.method.AllMethod;
+
 /**
  * Created by Administrator on 2017/6/25.
  */
 public class HelloGit {
     public static void main(String[] args) {
-        System.out.println("HelloGit!!");
+        AllMethod method = new AllMethod();
+        method.method1(git);
     }
 }
---
+++ hellogit/src/com/method/AllMethod.java
@@ -0,0 +1,10 @@
+package com.method;
+
+/**
+ * Created by Administrator on 2017/6/25.
+ */
+public class AllMethod {
+    public String method1(String name) {
+        return "你好" + name;
+    }
+}
