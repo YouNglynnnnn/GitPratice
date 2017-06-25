6ab4e929b08a84be058df764b96b8791c0b34834
yanglin <lyn00009@163.com>
2017-06-25 13:53:02 +0800

    first commit

---
+++ web_demo/src/Hello.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+/**
+ * Created by Administrator on 2017/6/11.
+ */
+
+public class Hello {
+    public static void main(String[] args) {
+        ArrayList<Object> list = new ArrayList<>();
+    }
+}
